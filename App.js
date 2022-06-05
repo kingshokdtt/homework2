67447d9e8cb7c5c81a45e8b38739e5a07b9f73dc
Kingshok <kingshok.datta@student.csulb.edu>
2022-06-05 12:39:20 -0700

    homework2

--- App.js
+++ App.js
@@ -1,15 +1,15 @@
+import React from "react"
 import { StatusBar } from 'expo-status-bar';
 import { SafeAreaView, StyleSheet, Text, View } from 'react-native';
-////
 
 export default function App() {
   return (
     <SafeAreaView>
-      <View style={{marginTop:100}}>
+      <View style={{marginTop:60}}>
 
       <StatusBar style="auto"/>
 
-    <View style={{paddingLeft:30, borderWidth:3, marginStart:50,marginEnd:770}}>
+    <View style={{paddingLeft:20, borderWidth:3, marginStart:50,marginEnd:180}}>
           <Text>Column</Text>
         
 
@@ -34,7 +34,7 @@ export default function App() {
       </View>
 
 
-      <View style={{paddingLeft:30, borderWidth:3,marginStart:50,marginEnd:560}}>
+      <View style={{paddingLeft:30, borderWidth:3,marginStart:50,marginEnd:15}}>
         <Text>Row</Text>
       
 
@@ -69,9 +69,8 @@ const styles=StyleSheet.create({
 
   boxContainer1:{
     
-    width:100,
-    height:320,
-    backgroundColor:'aliceblue',
+    width:80,
+    height:300,
     flexDirection:'column',
     justifyContent:'center',
     alignItems:'center',
@@ -81,9 +80,8 @@ const styles=StyleSheet.create({
 
   boxContainer2:{
     
-    width:300,
+    width:230,
     height:130,
-    backgroundColor:'aliceblue',
     flexDirection:'row',
     justifyContent:'center',
     alignItems:'center',
@@ -91,8 +89,8 @@ const styles=StyleSheet.create({
   },
 
   boxStyle: {
-    width:100,
-    height:100,
+    width:80,
+    height:80,
     justifyContent:'center',
     alignItems:'center'
   },
