b4d204a1636bae19e34fb1db0df282aa7167f0d0
Kingshok <kingshok.datta@student.csulb.edu>
2022-06-05 12:05:09 -0700

    homework2

--- App.js
+++ App.js
@@ -1,20 +1,104 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { SafeAreaView, StyleSheet, Text, View } from 'react-native';
+
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <SafeAreaView>
+      <View style={{marginTop:100}}>
+
+      <StatusBar style="auto"/>
+
+    <View style={{paddingLeft:30, borderWidth:3, marginStart:50,marginEnd:770}}>
+          <Text>Column</Text>
+        
+
+      <View style={styles.boxContainer1}>
+        
+        <View style={[styles.boxStyle, {borderWidth:3, backgroundColor:'aquamarine'}]}>
+            <Text>Child</Text>
+        </View>
+
+        <View style={[styles.boxStyle, {borderWidth:3,backgroundColor:'gray'}]}>
+            <Text>Child</Text>
+        </View>
+
+        <View style={[styles.boxStyle, {borderWidth:3,backgroundColor:'cyan'}]}>
+            <Text>Child</Text>
+          </View>
+        </View>
     </View>
+
+      <View>
+        <Text> </Text>
+      </View>
+
+
+      <View style={{paddingLeft:30, borderWidth:3,marginStart:50,marginEnd:560}}>
+        <Text>Row</Text>
+      
+
+      <View style={styles.boxContainer2}>
+        
+        <View style={[styles.boxStyle, {borderWidth:3,backgroundColor:'aquamarine'}]}>
+            <Text>Child</Text>
+        </View>
+
+        <View style={[styles.boxStyle, {borderWidth:3,backgroundColor:'gray'}]}>
+            <Text>Child</Text>
+        </View>
+
+        <View style={[styles.boxStyle, {borderWidth:3,backgroundColor:'cyan'}]}>
+            <Text>Child</Text>
+          </View>
+        </View>
+      </View>
+
+
+      </View>
+
+      
+   
+    </SafeAreaView>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+
+
+const styles=StyleSheet.create({
+
+  boxContainer1:{
+    
+    width:100,
+    height:320,
+    backgroundColor:'aliceblue',
+    flexDirection:'column',
+    justifyContent:'center',
+    alignItems:'center',
+
   },
+
+
+  boxContainer2:{
+    
+    width:300,
+    height:130,
+    backgroundColor:'aliceblue',
+    flexDirection:'row',
+    justifyContent:'center',
+    alignItems:'center',
+
+  },
+
+  boxStyle: {
+    width:100,
+    height:100,
+    justifyContent:'center',
+    alignItems:'center'
+  },
+
+
+
 });
+
+
